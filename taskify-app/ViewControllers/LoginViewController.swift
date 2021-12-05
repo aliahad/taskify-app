bca34d45fd45a14286538c8020c09f8269a5d3e9
Jignesh Kumavat <Jitukumavat234@gmail.com>
2021-12-05 16:36:27 -0500

    Login screen and added config file

--- taskify-app/ViewControllers/LoginViewController.swift
+++ taskify-app/ViewControllers/LoginViewController.swift
@@ -6,15 +6,112 @@
 //
 
 import UIKit
+import CoreData;
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userType: UISegmentedControl!
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    
+    @IBOutlet weak var userEmailErrorLabel: UILabel!
+    @IBOutlet weak var userPasswordErrorLabel: UILabel!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        initialize();
     }
     
+    func initialize() {
+        resetForm();
+    }
+    
+    @IBAction func loginUser(_ sender: Any) {
+        if(validate()){
+            let email = userEmail.text
+            let password = userPassword.text
+            
+            let request: NSFetchRequest<User> = User.fetchRequest()
+            request.fetchLimit = 1
+            request.predicate = NSPredicate(format: "email == %@ AND password = %@", email as! CVarArg, password as! CVarArg)
+            
+            do {
+                let user = try context.fetch(request)
+                if(user.first != nil){
+                    Configs.loggedINUserEmail = (user.first?.email)!
+                } else {
+                    print("Login Credentials not correct")
+                }
+            } catch {
+                print ("Error getting user")
+            }
+            
+        }
+    }
+    
+    private func resetForm() {
+        userEmail.text = ""
+        userPassword.text = ""
+        
+        resetErrors()
+    }
+    
+    private func resetErrors() {
+        userEmailErrorLabel.text = ""
+        userEmailErrorLabel.isHidden = true
+        
+        userPasswordErrorLabel.text = ""
+        userPasswordErrorLabel.isHidden = true
+    }
+    
+    private func validate() -> Bool {
+        resetErrors()
+        
+        var isValid = true;
+        
+        if (userEmail.text == "") {
+            userEmailErrorLabel.text = "*User email is required"
+            userEmailErrorLabel.isHidden = false
+            isValid = false
+        }
+        
+        if (userEmail.text != "" && !validateEmail()) {
+            userEmailErrorLabel.text = "*Invalid email"
+            userEmailErrorLabel.isHidden = false
+            userEmail.text = ""
+            isValid = false
+        }
+        
+        if (userPassword.text == "") {
+            userPasswordErrorLabel.text = "*User password is required"
+            userPasswordErrorLabel.isHidden = false
+            isValid = false
+        }
+        
+        return isValid
+    }
+    
+    private func validateEmail() -> Bool {
+        let emailPattern = #"^\S+@\S+\.\S+$"#
+        
+        let result = userEmail.text!.range(
+            of: emailPattern,
+            options: .regularExpression
+        )
+        
+        return (result != nil)
+    }
+        
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
