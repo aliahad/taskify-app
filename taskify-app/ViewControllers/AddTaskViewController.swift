35b220879e9105892aa0cffdc5aa50b5a4fe9070
Sohail Shiraj <sohail.shiraj@hotmail.com>
2021-12-06 16:07:42 -0500

    Added Add task screen

--- taskify-app/ViewControllers/AddTaskViewController.swift
+++ taskify-app/ViewControllers/AddTaskViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class AddTaskViewController: UIViewController {
 
+    @IBOutlet weak var taskTitle: UITextField!
+    @IBOutlet weak var taskDetail: UITextField!
+    @IBOutlet weak var taskHours: UITextField!
+    @IBOutlet weak var taskPayPerHour: UITextField!
+    @IBOutlet weak var taskStartDate: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.taskStartDate.datePicker(target: self,
+                                          doneAction: #selector(doneAction),
+                                          cancelAction: #selector(cancelAction),
+                                          datePickerMode: .date)
+    }
+    
+    @IBAction func addTask(_ sender: Any) {
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -23,6 +35,26 @@ class AddTaskViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    @objc
+        func cancelAction() {
+            self.taskStartDate.resignFirstResponder()
+        }
+
+    @objc
+    func doneAction() {
+        if let datePickerView = self.taskStartDate.inputView as? UIDatePicker {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let dateString = dateFormatter.string(from: datePickerView.date)
+            self.taskStartDate.text = dateString
+            
+            print(datePickerView.date)
+            print(dateString)
+            
+            self.taskStartDate.resignFirstResponder()
+        }
     }
 
     /*
@@ -35,4 +67,50 @@ class AddTaskViewController: UIViewController {
     }
     */
 
+}
+
+extension UITextField {
+    func datePicker<T>(target: T,
+                       doneAction: Selector,
+                       cancelAction: Selector,
+                       datePickerMode: UIDatePicker.Mode = .date) {
+        let screenWidth = UIScreen.main.bounds.width
+        
+        func buttonItem(withSystemItemStyle style: UIBarButtonItem.SystemItem) -> UIBarButtonItem {
+            let buttonTarget = style == .flexibleSpace ? nil : target
+            let action: Selector? = {
+                switch style {
+                case .cancel:
+                    return cancelAction
+                case .done:
+                    return doneAction
+                default:
+                    return nil
+                }
+            }()
+            
+            let barButtonItem = UIBarButtonItem(barButtonSystemItem: style,
+                                                target: buttonTarget,
+                                                action: action)
+            
+            return barButtonItem
+        }
+        
+        let datePicker = UIDatePicker(frame: CGRect(x: 0,
+                                                    y: 0,
+                                                    width: screenWidth,
+                                                    height: 216))
+        datePicker.datePickerMode = datePickerMode
+        self.inputView = datePicker
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: screenWidth,
+                                              height: 44))
+        toolBar.setItems([buttonItem(withSystemItemStyle: .cancel),
+                          buttonItem(withSystemItemStyle: .flexibleSpace),
+                          buttonItem(withSystemItemStyle: .done)],
+                         animated: true)
+        self.inputAccessoryView = toolBar
+    }
 }
