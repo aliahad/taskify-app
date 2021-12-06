86969d51eb3c2396cbdda9044a5e85f10323a278
Sohail Shiraj <sohail.shiraj@hotmail.com>
2021-12-05 23:38:55 -0500

    Added Requester Dashboard

---
+++ taskify-app/ViewControllers/RequesterDashboardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RequesterDashboardViewController.swift
+//  taskify-app
+//
+//  Created by user198241 on 12/5/21.
+//
+
+import UIKit
+
+class RequesterDashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
