8842ad6ef10ca0ae9f67b1750f05d685aa275332
Niraj Sutariya <nirajsutariya7@gmail.com>
2021-12-06 23:38:01 -0500

    Added send proposal screen and functionality

--- taskify-app/ViewControllers/ApplyTaskViewController.swift
+++ taskify-app/ViewControllers/ApplyTaskViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ApplyTaskViewController: UIViewController {
 
@@ -15,16 +16,69 @@ class ApplyTaskViewController: UIViewController {
     @IBOutlet weak var taskNumOfHours: UILabel!
     @IBOutlet weak var taskHourRate: UILabel!
     @IBOutlet weak var taskStartDate: UILabel!
+    @IBOutlet weak var taskLocation: UILabel!
     
     @IBOutlet weak var proposalDescription: UITextField!
+    
+    var task: Task? = nil
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        taskTitle.text = task?.title
+        taskDescription.text = task?.description
+        taskNumOfHours.text = String(Int16(task!.hours))
+        taskHourRate.text = String(Int16(task!.ratePerHour))
+        taskPostedBy.text = task?.requester?.name
+        taskStartDate.text = formatDate(date: (task?.startDate)!)
     }
     
     @IBAction func submitProposal(_ sender: UIButton) {
+        
+        let taskProposal = TaskProposal(context: context)
+        taskProposal.details = proposalDescription.text
+        taskProposal.status = "PENDING"
+        taskProposal.submissionDate = Date()
+        
+        taskProposal.task = task
+        taskProposal.tasker = fetchUser()
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error while saving proposal")
+        }
+        
+        task?.status = "PENDING"
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error while updating task")
+        }
+    }
+    
+    private func fetchUser() -> User? {
+        let request: NSFetchRequest<User> = User.fetchRequest()
+        request.fetchLimit = 1
+        request.predicate = NSPredicate(format: "email == %@", Configs.loggedInUserEmail)
+        
+        do {
+            let user = try context.fetch(request)
+            return user[0]
+        } catch {
+            print("Error while getting user")
+            return nil
+        }
+    }
+    
+    private func formatDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        
+        return formatter.string(from: date)
     }
 
     /*
